9625c74a5714f8e794498ef111722a7b4d8cba1b
Yuliia008 <uliagladkina@gmail.com>
2025-05-27 09:57:59 +0300

    feat: svelte app

---
+++ projektit/svelte-kauppa/_app/immutable/nodes/0.ORQlnjbB.js
@@ -0,0 +1,1 @@
+import{c as n,a}from"../chunks/CiUQHRez.js";import{i}from"../chunks/D4QbT_vd.js";import{s as p}from"../chunks/BR42bzLq.js";const s=!0,f=Object.freeze(Object.defineProperty({__proto__:null,prerender:s},Symbol.toStringTag,{value:"Module"}));function u(o,r){var e=n(),t=i(e);p(t,()=>r.children),a(o,e)}export{u as component,f as universal};
